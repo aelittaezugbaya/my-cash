40c32a868ca9b3089270809e256277b0b5520d0e
Aelitta Ezugbaya <aelittae@gmail.com>
2017-11-25 10:13:23 +0200

    set up project

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  frontend: path.join(__dirname, 'frontend'),
+  build: path.join(__dirname, 'public'),
+};
+
+
+const commonConfig= {
+  entry: {
+    frontend: PATHS.frontend
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My cash',
+      template: path.join(PATHS.frontend, 'template.html')
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude:/node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      },
+    ],
+  }
+};
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      historyApiFallback: true,
+      stats: 'errors-only',
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+      proxy: {
+        '/api/**': "http://localhost:3000/"
+      }
+    },
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  );
+};
+
+module.exports=(env)=> {
+  if (env === 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig()
+}
