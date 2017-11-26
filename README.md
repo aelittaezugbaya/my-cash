7124cc8731aca5966436752d66818464d2809c72
Sergey Ushakov <sergey.ushakov94@gmail.com>
2017-11-26 09:41:43 +0200

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # my-cash
+
+The aim of my-cash is to make budget and account management easier.
+
+## Guide: How to use my-cash
+* Firstly, app will ask for user's montly income
+* Then user will get to the main page, there is a list:
+  * Montly Income
+  * Unsorted spends
+  * Rest(Rest of montly income)
+  * Total amount of money on the account
+  * Savings(Amount of money which user not going to spend, just saving)
+* To add new "Fraction" click the button `Add fraction` and fill the form. User can add any amount of fractions
+* To sort a transacion to a fraction, open the unsorted spends and press `Sort` to choose what trancsaction will go to what fraction
+* Main page contains the pie chart on the left side, which shows how income was divided.
+
+## Why did we choose this idea?
+
+This idea came from having a problem with managing money on our account. Each month we make budget for whole month based on the income, but it is always difficult not to spent more than we have expected, because after each purchase we should calculate by ourself how much money we still have for food or for entertainment and so on. Such app will make it easier for us, and with open Bank API it will automatically receive and sort transactions. App will calculate money left on fraction based on previous transactions and we will see how much money we still have for what aim. Another aspect is to help to save money. Also we wanted to implement some achievements and make app more game-like to make saving money more fun but we ran out of time.
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Mongoose = require('Mongoose');
-const token = "Bearer eyJjdHkiOiJKV1QiLCJlbmMiOiJBMTI4R0NNIiwiYWxnIjoiZGlyIn0..sHrzcnihXjO4Vs5F.GVgAfJCCashlluYSq0TFzn-hZYkVzcxB7MGB3L2owSRy0KuGO3JxJmCMnqRewWHxEvLGxRqwEfiULRw43q4v7Vv46oMqRm-kKb8xcu2S2IdBLxBQGW3B_ieSLZZ46MARkoYN78tTdb1XqV2UWCTaMUkPPwvUQj0_LYuGjtZx7Vywoh2vWiIrG_LaYF2ogbC0DYIbkKO29w3kv5YkQH12Gb5rCLGWBCysga_C-uqKvRQ0TAUpCno8VhXB-uZiqReU8JXKgPEKG2Sr1j8V0u2pPl4JnDXE51DE5C61TZFePCZtjGYwfcFPB9_5ze_L1-kR5DsUpbg8M5WA9uF9tERp8NaFQtGT5yxrozFdimZbGnNjedfwHMGqsEiyjAaLVNgKYVv8sE-OXGtdRqzAQctEToARar4AYg0y0vfkv18rPA5PRSTRYBvLqFhMeOD2TqJgGGPBWYTtBUBya6jJ55gH4SqE9rKGr5m00XKfVhIrFFmxxvt7N-JJEPRz9RdYoGDoZfIbv1HhztTroIBWVXtF35BHnMdSFkZtFrvUMynL1I8R_cJDT9RuZgFJYobZqt4RyXWkNx80tKucqQ8d7EQupi-f-RejfehaG8ND3aFyuk1U8mTPNs46boJ_QohMBjj6NExA2Pg7PbpwxYJ1dqmkXQUraaVDjjUOJTpUtrAxxj7KBu852sPkuy-_CDKSiQhRQjZ_KBGUtMmEyApaBiN19hItx0S0AEOMq2iQd_GPqhSfvE5qLq2u7zSptCJwOJjH0JFX9qGgdRWC7PzHBYBXzcdQkagSop-b9N81Uxba6w-K2GXksAhH8Pl1NTeAJCFypgHabetwbh5VEUjdDX2vSoB6yf2Skj9jT3TJOfHQZ2T9EsQZ21v824CRnq0yzS7Xg97GaV0YgXyy7boPL-sMFq3m_bvGBu4LgkRZ96-G5ejbrCG3MO62ID824yexiVo0Q0CaNYZUN6RN8kYePTp-L3cf4UTVrmybXHIZhXaeMocfzYXUxPSQRWM7OzU3SvrCez3d2u4jQXYXV_yYnt_SO6bEeww3FahmwEa8QARmnAAgV60_4I4Q05JKDvsI-yGK_Ry4xYcSG7wDLbL-cYglsmh1l8x53keOvqlzuL0bJwhhqycIeu_ewTi7JLfKGXJefpgTmQqo8AwmmE6W1xDbvaVtXcx1i3l1Dl9LwN7SMaA.C-ZQnA2TxTgZJ1xaQfjH4A";
+const token = require('../../.secret.js')[1]
 const fetch = require('node-fetch');
 
 const debtorSchema = Mongoose.Schema({
@@ -51,8 +51,6 @@ router.post('/fractions', function(req, res) {
     .then(() => res.send('kek'));
 });
 
-
-
 router.delete('/fractions/:id',function(req,res){
   console.log('deleting item', req.params.id);
   fractionModel.remove({_id:req.params.id}, (err,data)=>{
@@ -72,7 +70,7 @@ router.get('/transactions', function(req, res){
       headers: {
         Authorization: token,
         'x-ibm-client-id': '3c25e72a-0d80-4e9a-9b2c-fe3e9a726e32',
-        'X-IBM-Client-Secret': require('../../.secret.js'),
+        'X-IBM-Client-Secret': require('../../.secret.js')[0],
         Accept: 'application/json'
       }
       }).then(data => data.json())
